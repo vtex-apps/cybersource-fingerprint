da6bf2327bf472365f519af13c08738257cb8492
Breno Calazans <brenocalazans@gmail.com>
2020-02-13 16:12:21 -0300

    Initial implementation

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,7 @@
 First of all (even before the app's name), its README.md should contain the following sentence at the very beginning:
+<!-- ALL-CONTRIBUTORS-BADGE:START - Do not remove or modify this section -->
+[![All Contributors](https://img.shields.io/badge/all_contributors-0-orange.svg?style=flat-square)](#contributors-)
+<!-- ALL-CONTRIBUTORS-BADGE:END -->
 
 `📢 Use this project, [contribute](https://github.com/{OrganizationName}/{AppName}) to it or open issues to help evolve it using [Store Discussion](https://github.com/vtex-apps/store-discussion).`
 
@@ -24,3 +27,16 @@ If you feel compelled to give further details, such as the app's **relationship 
 
 ---
 
+
+## Contributors ✨
+
+Thanks goes to these wonderful people ([emoji key](https://allcontributors.org/docs/en/emoji-key)):
+
+<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
+<!-- prettier-ignore-start -->
+<!-- markdownlint-disable -->
+<!-- markdownlint-enable -->
+<!-- prettier-ignore-end -->
+<!-- ALL-CONTRIBUTORS-LIST:END -->
+
+This project follows the [all-contributors](https://github.com/all-contributors/all-contributors) specification. Contributions of any kind welcome!
--- react/index.tsx
+++ react/index.tsx
@@ -1,10 +1,70 @@
 import { canUseDOM } from 'vtex.render-runtime'
 import { PixelMessage } from './typings/events'
 
+interface Settings {
+  orgId: string
+  merchantId: string
+}
+
+type SessionId = string
+
+interface Params extends Settings {
+  sessionId: SessionId
+}
+
+function createPixelP({ orgId, merchantId, sessionId }: Params) {
+  const p = document.createElement('p')
+  p.setAttribute('style', `background:url(https://h.online-metrix.net/fp/clear.png?org_id=${orgId}&amp;session_id=${merchantId}${sessionId}&amp;m=1)`)
+  document.body.appendChild(p)
+}
+
+function createPixelImg({ orgId, merchantId, sessionId }: Params) {
+  const img = document.createElement('img')
+  img.setAttribute('src', `https://h.online-metrix.net/fp/clear.png?org_id=${orgId}&amp;session_id=${merchantId}${sessionId}&amp;m=2`)
+  document.body.appendChild(img)
+}
+
+function createPixelObject({ orgId, merchantId, sessionId }: Params) {
+  const objElement = document.createElement('object')
+  objElement.setAttribute('type', 'application/x-shockwave-flash')
+  objElement.setAttribute('data', `https://h.online-metrix.net/fp/fp.swf?org_id=${orgId}&amp;session_id=${merchantId}${sessionId}`)
+  objElement.setAttribute('width', '1')
+  objElement.setAttribute('height', '1')
+  objElement.setAttribute('id', 'thm_fp')
+  const param = document.createElement('param')
+  param.setAttribute('name', 'movie')
+  param.setAttribute('value', `https://h.online-metrix.net/fp/fp.swf?org_id=${orgId}&amp;session_id=${merchantId}${sessionId}`)
+  objElement.appendChild(param)
+  const div = document.createElement('div')
+  objElement.appendChild(div)
+  document.body.appendChild(objElement)
+}
+
+function createPixelScript({ orgId, merchantId, sessionId }: Params) {
+  const scriptElement = document.createElement('script')
+  scriptElement.setAttribute('type', 'text/javascript')
+  scriptElement.setAttribute('src', `https://h.online-metrix.net/fp/check.js?org_id=${orgId}&amp;session_id=${merchantId}${sessionId}`)
+  document.body.appendChild(scriptElement)
+}
+
+function createPixel(params: Params) {
+  createPixelP(params)
+  createPixelImg(params)
+  createPixelObject(params)
+  createPixelScript(params)
+}
+
 export function handleEvents(e: PixelMessage) {
   switch (e.data.eventName) {
-    case 'vtex:pageView': {
+    case 'vtex:orderPlaced': {
+      const { transactionPayment: { id }} = e.data
 
+      createPixel({
+        orgId: window.__cybersource__.orgId,
+        merchantId: window.__cybersource__.merchantId,
+        sessionId: id,
+      })
+      return
     }
     default: {
       return
